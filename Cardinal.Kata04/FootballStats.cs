74d27a7c16aebdc63542eb1faf5ea68fb562a3d0
rodney <smithrod5000@gmail.com>
2016-05-31 20:45:23 -0400

    missed this file somehow on last push

---
+++ Cardinal.Kata04/FootballStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardinal.Kata04
+{
+    public class FootballStats
+    {
+        public string TeamName { get; set; }
+        public int For { get; set; }
+        public int Against { get; set; }
+    }
+}
